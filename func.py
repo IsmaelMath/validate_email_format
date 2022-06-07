e64a58328afbba6d927c3f44faa8e7ea2feae9fc
Ismael Batista <ismaelbatista3462@hotmail.com>
2022-06-07 16:17:47 -0300

    p01 - Função em python para o gramada de juros simples e juros compostos

---
+++ func.py
@@ -0,0 +1,18 @@
+
+def js(capital_inicial, juros, tempo):
+    montante = capital_inicial + capital_inicial * (juros / 100) * tempo
+    print(f"O capital no estante t{tempo}: R$ {montante: .2f}")
+    return montante
+
+
+def jc(capital_inicial, juros, tempo):
+    montante = capital_inicial * (1 + juros / 100) ** tempo
+    print(f"O capital no estante t{tempo}: R$ {montante: .2f}")
+    return montante
+
+
+def calcular(x, capital_inicial, juros, tempo):
+    if x.lower() == "js":
+        js(capital_inicial, juros, tempo)
+    else:
+        jc(capital_inicial, juros, tempo)
