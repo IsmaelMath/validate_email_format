e9511e861351a12ed1c05b2ed81e6fea49905f35
Ismael Batista <ismaelbatista3462@hotmail.com>
2022-06-17 13:27:53 -0300

    funcmed.py

--- funcmed.py
+++ funcmed.py
@@ -5,12 +5,13 @@ def media(lista_numeros, periodo):
         calculo_media = sum(lista_numeros[x: x+periodo]) / periodo
         media_dinamica.append(calculo_media)
 
-        if x+(periodo-1) >= len(lista_numeros):
+        if x+periodo >= len(lista_numeros):
             break
-    print(f"A media de periodo {periodo} e:  {media_dinamica}")
 
+    print(f"A media de periodo {periodo} e:  {media_dinamica}")
+    return media_dinamica
 
 lista = [25, 35, 45, 77, 85, 78, 48, 36, 49, 100]
-periodo = int(input("Informe o perido que deseja calcular a média : "))
+periodo = int(input("Informe o perido que deseja calcular na media"))
 
 w = media(lista, periodo) 
